2fef240119d7b89e2742aa06732e6c231a84f5c9
MDarowny <michal.darowny@gmail.com>
2022-03-09 02:41:59 +0100

    auth basic

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	cloud.google.com/go/datastore v1.6.0
 	github.com/lithammer/shortuuid/v4 v4.0.0
+	golang.org/x/crypto v0.0.0-20220307211146-efcb8507fb70
 	google.golang.org/appengine v1.6.7
 )
--- zaproszenia.go
+++ zaproszenia.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"zaproszenia/models"
+	"zaproszenia/utils"
 
 	"cloud.google.com/go/datastore"
 	"google.golang.org/appengine"
@@ -28,8 +29,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	http.HandleFunc("/", getAllInvit)
-	http.HandleFunc("/create", createInvit)
+	http.HandleFunc("/", basicAuth(getAllInvit))
+	http.HandleFunc("/create", basicAuth(createInvit))
 	appengine.Main()
 }
 
@@ -89,4 +90,35 @@ func createInvit(w http.ResponseWriter, r *http.Request) {
 	}
 	data, _ := json.Marshal(invit)
 	_, err = w.Write(data)
+}
+
+func basicAuth(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Extract the username and password from the request
+		// Authorization header. If no Authentication header is present
+		// or the header value is invalid, then the 'ok' return value
+		// will be false.
+		username, password, ok := r.BasicAuth()
+		if ok {
+			// Calculate SHA-256 hashes for the provided and expected
+			// usernames and passwords.
+			passwordMatch := utils.CheckPasswordHash(password, os.Getenv("AUTH_PASSWORD"))
+			usernameMatch := utils.CheckPasswordHash(username, os.Getenv("AUTH_USERNAME"))
+
+			// If the username and password are correct, then call
+			// the next handler in the chain. Make sure to return
+			// afterwards, so that none of the code below is run.
+			if usernameMatch && passwordMatch {
+				next.ServeHTTP(w, r)
+				return
+			}
+		}
+
+		// If the Authentication header is not present, is invalid, or the
+		// username or password is wrong, then set a WWW-Authenticate
+		// header to inform the client that we expect them to use basic
+		// authentication and send a 401 Unauthorized response.
+		w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+	})
 }
